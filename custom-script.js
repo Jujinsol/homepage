98ccd8d0bfd737f236101429df1d91e4f211193c
주진성 <jjsair0412@naver.com>
2023-10-05 19:16:02 +0900

    Modify button listener

--- custom-script.js
+++ custom-script.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     // 버튼 클릭 시 알림 메시지 표시
     const alertButton = document.getElementById("alert-button");
-    alertButton.addEventListener("click", function() {
+/*     alertButton.addEventListener("click", function() {
         alert("Button clicked!");
+    }); */
+
+    // "Learn more" 버튼 클릭 시 learn.html로 이동
+    alertButton.addEventListener("click", function() {
+        window.location.href = "learn-more.html";
     });
 
     // 이미지 변경
